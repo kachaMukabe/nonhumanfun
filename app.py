3ab166debf8c4e5b66e579ab7ec2cc681ec0d3bf
kachaMukabe <kachamukabe@gmail.com>
2021-12-04 21:00:12 +0200

    Working application with ai generated greeting card and loom recording

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+import pprint
+
+from flask import Flask, render_template, render_template_string
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get-greeting/<start_str>", methods=['POST'])
+def get_greeting(start_str):
+    templ = """
+    <div class="block">
+        <p>{{ greeting }}</p>
+    </div>
+    """
+    r = requests.post(
+            "https://api.deepai.org/api/text-generator",
+            data = {
+                "text": start_str 
+                },
+            headers={ 'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K' }
+            )
+    pprint.pprint(r)
+    response = r.json()
+    return render_template_string(templ, greeting=response["output"])
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
